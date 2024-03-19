adbf3133b8c1ca08515cfc64da42307985e62378
Nikhil Lokare <110295778+nikhillokare@users.noreply.github.com>
2024-03-19 13:35:38 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -39,94 +39,73 @@ export const NavBar = () => {
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link
-                href="#home"
-                className={
-                  activeLink === "home" ? "active navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("home")}
-              >
-                Home
-              </Nav.Link>
-              <Nav.Link
-                href="#skills"
-                className={
-                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("skills")}
-              >
-                Skills
-              </Nav.Link>
-              <Nav.Link
-                href="#projects"
-                className={
-                  activeLink === "projects"
-                    ? "active navbar-link"
-                    : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("projects")}
-              >
-                Services & Projects
-              </Nav.Link>
-                   <Nav.Link
-                href="https://promptbase.com/profile/artistrybynikhil"
-                target="_blank"
-                className={
-                  activeLink === "promptbase"
-                    ? "active navbar-link"
-                    : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("promptbase")}
-              >
-                PromptBase
-              </Nav.Link>
-              <Nav.Link
-                href="https://www.youtube.com/@MrNikhil11"
-                target="_blank"
-                className={
-                  activeLink === "youtube"
-                    ? "active navbar-link"
-                    : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("youtube")}
-              >
-                YouTube
-              </Nav.Link>
-              <Nav.Link
-                href="https://github.com/nikhillokare"
-                target="_blank"
-                className={
-                  activeLink === "github" ? "active navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("github")}
-              >
-                GitHub
-              </Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-              <div className="social-icon">
-                <a
-                  href="https://www.linkedin.com/in/nikhil-lokare-b45930205/"
-                  target="_blank"
-                >
-                  <img src={navIcon1} alt="" />
-                </a>
-                <a
-                  href="https://www.facebook.com/nikhil.lokare.503/"
-                  target="_blank"
-                >
-                  <img src={navIcon2} alt="" />
-                </a>
-                <a
-                  href="https://www.instagram.com/artistrybynikhil24/"
-                  target="_blank"
-                >
-                  <img src={navIcon3} alt="" />
-                </a>
-              </div>
-            </span>
-          </Navbar.Collapse>
+            <Navbar.Collapse id="basic-navbar-nav">
+  <Nav className="ms-auto">
+    <Nav.Link
+      href="#home"
+      className={activeLink === "home" ? "active navbar-link" : "navbar-link"}
+      onClick={() => onUpdateActiveLink("home")}
+    >
+      Home
+    </Nav.Link>
+    <Nav.Link
+      href="#skills"
+      className={activeLink === "skills" ? "active navbar-link" : "navbar-link"}
+      onClick={() => onUpdateActiveLink("skills")}
+    >
+      Skills
+    </Nav.Link>
+    <Nav.Link
+      href="#projects"
+      className={activeLink === "projects" ? "active navbar-link" : "navbar-link"}
+      onClick={() => onUpdateActiveLink("projects")}
+    >
+      Services & Projects
+    </Nav.Link>
+    <Nav.Link
+      href="https://promptbase.com/profile/artistrybynikhil"
+      target="_blank"
+      className={activeLink === "promptbase" ? "active navbar-link" : "navbar-link"}
+      onClick={() => onUpdateActiveLink("promptbase")}
+    >
+      PromptBase
+    </Nav.Link>
+    <Nav.Link
+      href="https://www.youtube.com/@MrNikhil11"
+      target="_blank"
+      className={activeLink === "youtube" ? "active navbar-link" : "navbar-link"}
+      onClick={() => onUpdateActiveLink("youtube")}
+    >
+      YouTube
+    </Nav.Link>
+    <Nav.Link
+      href="https://github.com/nikhillokare"
+      target="_blank"
+      className={activeLink === "github" ? "active navbar-link" : "navbar-link"}
+      onClick={() => onUpdateActiveLink("github")}
+    >
+      GitHub
+    </Nav.Link>
+  </Nav>
+  <Nav className="mx-auto">
+    <Nav.Item>
+      <span className="navbar-text">
+        <div className="social-icon">
+          <a href="https://www.linkedin.com/in/nikhil-lokare-b45930205/" target="_blank">
+            <img src={navIcon1} alt="" />
+          </a>
+          <a href="https://www.facebook.com/nikhil.lokare.503/" target="_blank">
+            <img src={navIcon2} alt="" />
+          </a>
+          <a href="https://www.instagram.com/artistrybynikhil24/" target="_blank">
+            <img src={navIcon3} alt="" />
+          </a>
+        </div>
+      </span>
+    </Nav.Item>
+  </Nav>
+</Navbar.Collapse>
+
         </Container>
       </Navbar>
     </Router>
